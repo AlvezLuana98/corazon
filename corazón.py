25da7dc50ff92369af18831f22e5c666d22fcc74
Luana Alvez <78671652+AlvezLuana98@users.noreply.github.com>
2022-01-27 03:59:43 -0300

    Add files via upload

---
+++ corazón.py
@@ -0,0 +1,15 @@
+from turtle import*
+
+def curvemove():
+	for i in range(200):
+		right(1)
+		forward(1)
+
+color('red','pink')
+begin_fill()
+left(140)
+forward(111.65)
+curvemove()
+forward(112.65)
+end_fill()
+done()
